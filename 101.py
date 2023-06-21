991705e64c2246b246e1a3d22e2122c5db31d1fe
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2023-06-21 13:56:14 +0100

    better structure with prefect.yaml

--- 101.py
+++ 101.py
@@ -10,6 +10,7 @@ LOCATIONS = {
     "london": (51.5, -0.12),
 }
 
+
 @task(retries=3,cache_key_fn=task_input_hash, cache_expiration=datetime.timedelta(minutes=1))
 def fetch_rain(lat: float, lon: float):
     base_url = "https://api.open-meteo.com/v1/forecast/"
@@ -19,6 +20,7 @@ def fetch_rain(lat: float, lon: float):
     )
     most_recent_rain = float(weather.json()["hourly"]["rain"][0])
     return most_recent_rain
+
 
 @task(retries=3,cache_key_fn=task_input_hash, cache_expiration=datetime.timedelta(minutes=1))
 def fetch_cloud(lat: float, lon: float):
@@ -30,6 +32,7 @@ def fetch_cloud(lat: float, lon: float):
     most_recent_cloudcover = float(weather.json()["hourly"]["cloudcover"][0])
     return most_recent_cloudcover
 
+
 @task(log_prints=True)
 def tell_me_its_crap(loc: str):
     with open("weather.csv", "w+") as w:
@@ -37,8 +40,9 @@ def tell_me_its_crap(loc: str):
         writer.writerow([loc, datetime.datetime.now()])
     print(f"it's crap weather in {loc}")
 
-@flow
-def pipeline(loc: str, lat: float, lon: float):
+
+@flow(name="get some weather")
+def get_weather(loc: str, lat: float, lon: float):
     LOGGER = get_run_logger()
     LOGGER.info(f"Fetching weather for {loc}")
     rain = fetch_rain(lat, lon)
@@ -46,7 +50,10 @@ def pipeline(loc: str, lat: float, lon: float):
     if rain > 1 or cloud > 90:
         tell_me_its_crap(loc)
 
+@flow(name="getting all the weather")
+def run_pipe(locations_dict: dict = LOCATIONS):
+    for k, v in locations_dict.items():
+        get_weather(k, v[0], v[1])
 
 if __name__ == "__main__":
-    for k, v in LOCATIONS.items():
-        pipeline(k, v[0], v[1])
+    run_pipe()
